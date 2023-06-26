0bccbe5ede4fe4e8c9fbaca096edb7058b33212f
Michal Szemryk <szemrykmichal@gmail.com>
2023-06-26 16:26:29 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	const nav = document.querySelector('.navbar');
 
 	addShadow = () => {
-		if (window.scrollY >= 300) {
+		if (window.scrollY >= 50) {
 			nav.classList.add('shadow-bg');
 		} else {
 			nav.classList.remove('shadow-bg');
